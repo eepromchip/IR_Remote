df95969814cd081c4d3b58ca6f4daf9584a36a51
https://github.com/eepromchip <eepromchip@gmail.com>
2018-11-27 09:05:18 -0500

    Removed LIB ref in ini file. Added lib to irMain

--- src/IRMain.ino
+++ src/IRMain.ino
@@ -1,12 +1,13 @@
 // This code is for the IR emitter Module
 // install the library before uploading the code
+#include <Arduino.h>
 #include < IRremote.h >
 
 IRsend irsend ;
 
 void setup( )
 {
-irsend.sendNEC(0x20DF10EF, 32); // This command is for POWER for Vizio 
+irsend.sendNEC(0x20DF10EF, 32); // This command is for POWER for Vizio
 }
 
 void loop( ) {
